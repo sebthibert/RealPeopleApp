4b3a2d9025cb403feb3410037480f03a40718e04
Sebastien Thibert <sebastien.thibert@marks-and-spencer.com>
2018-02-25 18:33:07 +0000

    Update UI and mock data

--- RealPeople/Extras/LayoutHelper.swift
+++ RealPeople/Extras/LayoutHelper.swift
@@ -3,5 +3,5 @@ import UIKit
 struct LayoutHelper {
   static let galleryCellMargin: CGFloat = 10
   static let galleryColumnCount: CGFloat = 2
-  static let galleryCellHeight: CGFloat = 50
+  static let galleryCellHeight: CGFloat = 40
 }
